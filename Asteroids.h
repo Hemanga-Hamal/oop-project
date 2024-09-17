8829c3eb6d548b16b7c1904bb6868a2c613a6915
aisihco <caoxxker@gmail.com>
2024-09-17 18:24:28 +0930

    Fixing Enemy.h parameter acceptance types, and finished Asteroids.h and cpp (except for function setEnemySpeed())

--- Asteroids.h
+++ Asteroids.h
@@ -8,18 +8,22 @@ class Asteroids: protected Enemy{
         int asteroids_damage;
     public:
         // Constructor
-        Asteroids(int, int);
-
-        // Scaler
-        void asteroidsStatsScale(int, int);
+        Asteroids(int, int, float, float, float, float, float, int);
 
         // Setters
         void setAsteroidsDamage(int);
         void setAsteroidsScale(int);
+        void setEnemyHealth(int);
+        void setEnemySpeed(float, float);
+        void setEnemyPos(Vector2);
+        void setEnemyRotation(float);
 
         // Getters
         int getAsteroidsDamage();
         int getAsteroidsScale();
+
+        // Movement
+        void movement(float, float);
 
         // Updating and Rendering
         void draw();
