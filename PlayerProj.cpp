c093413f2227e6bab03b235acc833a0415f711fa
ThisCouch <jonahdevizio@gmail.com>
2024-09-20 18:02:51 +0930

    playerproj.cpp

--- PlayerProj.cpp
+++ PlayerProj.cpp
@@ -1,11 +1,23 @@
-#include "raylib.h"
+#include "raymath.h"
 #include "PlayerProj.h"
+#include <cmath>
 
 // setters 
 void PlayerProj::setProjDamage(int damage)     {damage = proj_damage;}
 void PlayerProj::setProjSpeed(Vector2 speed)     {speed = proj_speed;}
 void PlayerProj::setProjPos(Vector2 pos)             {pos = proj_pos;}
 
-void 
-    proj_pos.x += proj_speed.x *deltatime;
-    proj_pos.y += proj_speed.y *deltatime;
+void PlayerProj::update(float deltaTime){
+      proj_pos.x += proj_speed.x *deltaTime;
+    proj_pos.y += proj_speed.y *deltaTime;
+}
+
+void PlayerProj::draw() {
+    float ProjLength = 10.0f;
+    
+    Vector2 ProjEnd = {
+        proj_pos.x + proj_speed.x * (ProjLength / Vector2Length(proj_speed)),
+        proj_pos.y + proj_speed.y * (ProjLength / Vector2Length(proj_speed))
+    };
+    DrawLineV(proj_pos, ProjEnd, PlayerProjCol);
+}
