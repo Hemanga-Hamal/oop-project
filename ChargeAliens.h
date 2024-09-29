80107f22d9558cde98dce16f5ec2614b0bffcaec
ThisCouch <jonahdevizio@gmail.com>
2024-09-29 16:39:37 +0930

    chargealiens

---
+++ ChargeAliens.h
@@ -0,0 +1,26 @@
+#ifndef CHARGEALIENS_H
+#define CHARGEALIENS_H
+
+
+#include "ScoutAlien.h"
+
+class ChargeAliens : public ScoutAlien{
+private: 
+    
+
+
+public:
+
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif
